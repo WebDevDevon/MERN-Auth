7a08fcb523d58bcf6c76b77a76a2c8ec2d9a1e1f
Devon <dbatemon33@gmail.com>
2022-10-28 20:09:44 -0700

    update

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
 require('dotenv').config()
 
 const express = require('express')
+
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
 const cors = require('cors')
 
 // express app
-const app = express()
+
 app.use(cors({
   origin: "*",
   methods: ["GET","POST","PUT","DELETE"],
