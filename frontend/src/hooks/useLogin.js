6adb5fb8dedd60aa9c8fa18aa5e4d37d1ebe6b37
Devon <dbatemon33@gmail.com>
2022-10-28 20:11:25 -0700

    update

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -10,7 +10,7 @@ export const useLogin = () => {
     setIsLoading(true)
     setError(null)
 
-    const response = await fetch('https://mern-auth-cpf9jklmp-webdevdevon.vercel.app/api/user/login', {
+    const response = await fetch('http://mern-auth-cpf9jklmp-webdevdevon.vercel.app/api/user/login', {
       method: 'POST',
       headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
       body: JSON.stringify({ email, password })
