d2d01ecf2ca042ddac0fa3f0cd886e5e45f379c4
Devon <dbatemon33@gmail.com>
2022-10-28 19:54:50 -0700

    update

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,11 @@ app.use((req, res, next) => {
 app.use('/api/workouts', workoutRoutes)
 app.use('/api/user', userRoutes)
 
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
