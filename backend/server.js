80eeadfdb0c63477ef06d970b5db6338f0fa3b58
Devon <dbatemon33@gmail.com>
2022-10-28 22:39:39 -0700

    update

--- backend/server.js
+++ backend/server.js
@@ -20,10 +20,12 @@ app.use(cors({
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  // res.header(
+  //   "Access-Control-Allow-Headers",
+  //   "Origin, X-Requested-With, Content-Type, Accept"
+  // );
   next();
 })
 
