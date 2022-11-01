2c555d94b1374df6107a8003eec5468d58585a92
Devon <dbatemon33@gmail.com>
2022-11-01 00:40:19 -0700

    fix

--- backend/server.js
+++ backend/server.js
@@ -20,18 +20,18 @@ app.use(cors({
 
 // credentials: true,
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Credentials", "true")
-//   res.header("Access-Control-Allow-Methods", "*")
-//   // res.header("Access-Control-Allow-Methods", "*");
-//   // res.header("Access-Control-Allow-Headers", "*");
-//   // res.header(
-//   //   "Access-Control-Allow-Headers",
-//   //   "Origin, X-Requested-With, Content-Type, Accept"
-//   // );
-//   next();
-// })
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Credentials", "true")
+  // res.header("Access-Control-Allow-Methods", "*")
+  // res.header("Access-Control-Allow-Methods", "*");
+  // res.header("Access-Control-Allow-Headers", "*");
+  // res.header(
+  //   "Access-Control-Allow-Headers",
+  //   "Origin, X-Requested-With, Content-Type, Accept"
+  // );
+  next();
+})
 
 // routes
 app.use('/api/workouts', workoutRoutes)
