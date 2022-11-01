c67535af971306ae009e124471ba8596597a266e
Devon <dbatemon33@gmail.com>
2022-11-01 00:26:59 -0700

    fix

--- backend/server.js
+++ backend/server.js
@@ -15,21 +15,23 @@ app.use(express.json())
 app.use(cors({
   origin: "*",
   methods: "*",
-  credentials: true,
+
 }))
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true")
-  res.header("Access-Control-Allow-Methods", "*")
-  // res.header("Access-Control-Allow-Methods", "*");
-  // res.header("Access-Control-Allow-Headers", "*");
-  // res.header(
-  //   "Access-Control-Allow-Headers",
-  //   "Origin, X-Requested-With, Content-Type, Accept"
-  // );
-  next();
-})
+// credentials: true,
+
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Credentials", "true")
+//   res.header("Access-Control-Allow-Methods", "*")
+//   // res.header("Access-Control-Allow-Methods", "*");
+//   // res.header("Access-Control-Allow-Headers", "*");
+//   // res.header(
+//   //   "Access-Control-Allow-Headers",
+//   //   "Origin, X-Requested-With, Content-Type, Accept"
+//   // );
+//   next();
+// })
 
 // routes
 app.use('/api/workouts', workoutRoutes)
